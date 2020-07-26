6f50d562fa261db3ba62afa4b6c91523424c772c
Cosmin Stoian <cosmyn9708@gmail.com>
2020-07-26 23:24:04 +0300

    Implemented the dark background theme on the SplashScreen as well

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sudoku_plus/pages/splashscreen_page.dart';
 
-import 'pages/login_page.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,53 +11,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           primarySwatch: Colors.blue,
           primaryColor: Color(0xffffa500),
+          accentColor: Color(0xffffa500),
           backgroundColor: Colors.black),
       home: SplashScreenPage(),
     );
   }
 }
-
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Sudoku Plus',
-//       theme: ThemeData(
-//         primarySwatch: Colors.cyan,
-//       ),
-//       home: GameMenu(),
-//     );
-//   }
-// }
-
-// class HomePage extends StatefulWidget {
-//   HomePage() : super();
-
-//   @override
-//   _HomePageState createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: Text("Sudoku Plus"),
-//         ),
-//         body: Center(
-//           child: Row(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: <Widget>[
-//               RaisedButton(
-//                 onPressed: () {
-//                   Navigator.push(context,
-//                       MaterialPageRoute(builder: (context) => GameMenu()));
-//                 },
-//                 child: Text("New Game"),
-//               )
-//             ],
-//           ),
-//         ));
-//   }
-// }
--- lib/pages/splashscreen_page.dart
+++ lib/pages/splashscreen_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:sudoku_plus/pages/home_page.dart';
@@ -14,26 +16,32 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
   }
 
   Future<void> _redirect() async {
-    final user = await FirebaseAuth.instance.currentUser();
-    if (user != null) {
-      Navigator.pushReplacement(context,
-          MaterialPageRoute(builder: (BuildContext context) => HomePage()));
-    } else {
-      Navigator.pushReplacement(context,
-          MaterialPageRoute(builder: (BuildContext context) => LoginPage()));
-    }
+    Future.delayed(Duration(seconds: 3)).then((value) async {
+      final user = await FirebaseAuth.instance.currentUser();
+      if (user != null) {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (BuildContext context) => HomePage()));
+      } else {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (BuildContext context) => LoginPage()));
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          brightness: Brightness.dark,
+          backgroundColor: Colors.black,
+        ),
+        backgroundColor: Colors.black,
         body: Container(
-            color: Colors.white,
             child: Center(
                 child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[CircularProgressIndicator()],
-            ))));
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[CircularProgressIndicator()],
+        ))));
   }
 }
